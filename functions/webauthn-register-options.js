c742046431837a317848add25772227bd0f41e33
udaydotoffical-svg <uday.dot.offical@gmail.com>
2026-07-18 22:22:14 +0530

    Refactor store function and simplify options generation

--- functions/webauthn-register-options.js
+++ functions/webauthn-register-options.js
@@ -1,27 +1,33 @@
 const { generateRegistrationOptions } = require('@simplewebauthn/server');
 const { getStore } = require('@netlify/blobs');
 
+function store() {
+    return getStore({
+        name: "webauthn",
+        siteID: process.env.NETLIFY_SITE_ID,
+        token: process.env.NETLIFY_BLOBS_TOKEN
+    });
+}
+
 exports.handler = async () => {
-  const store = getStore("webauthn");
+    const options = await generateRegistrationOptions({
+        rpName: "Knowura",
+        rpID: process.env.RP_ID,
+        userID: new TextEncoder().encode("uday-owner"),
+        userName: "uday",
+        attestationType: "none",
+        authenticatorSelection: {
+            authenticatorAttachment: "cross-platform",
+            userVerification: "preferred",
+            residentKey: "preferred"
+        }
+    });
 
-  const options = await generateRegistrationOptions({
-    rpName: "Knowura",
-    rpID: process.env.RP_ID,
-    userID: new TextEncoder().encode("uday-owner"),
-    userName: "uday",
-    attestationType: "none",
-    authenticatorSelection: {
-      authenticatorAttachment: "cross-platform",
-      userVerification: "preferred",
-      residentKey: "preferred"
-    }
-  });
+    await store().setJSON("current-challenge", { challenge: options.challenge });
 
-  await store.setJSON("current-challenge", { challenge: options.challenge });
-
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(options)
-  };
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(options)
+    };
 };
